653b66c22b7171615bc2fa7c13a58c26bf06464f
C00253239 <C00253239@LLS0291.home>
2021-01-06 16:40:06 +0000

    Added initial class

---
+++ Lab 1/src/lab1/Book.java
@@ -0,0 +1,116 @@
+package lab1;
+
+public class Book {
+	
+	//Variables
+	private String title;
+	private String author;
+	private int noOfPages;
+	private int edition;
+	private int chapters;
+	private char status;
+	
+	//Constructors
+	public Book() {
+		
+	}
+	
+	public Book(String title, String author, int noOfPages, int edition, int chapters, char status){
+		setTitle(title);
+		setAuthor(author);
+		setNoOfPages(noOfPages);
+		setEdition(edition);
+		setChapters(chapters);
+		setStatus('A');
+	}
+	
+	public Book(String title, String author, int noOfPages, int edition, char status){
+		setTitle(title);
+		setAuthor(author);
+		setNoOfPages(noOfPages);
+		setEdition(edition);
+		setStatus('A');
+	}
+	
+	public Book(String title, String author, int noOfPages,char status){
+		setTitle(title);
+		setAuthor(author);
+		setNoOfPages(noOfPages);
+		setStatus('A');
+	}
+	
+
+	
+	//Getters & Setters
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+	public int getNoOfPages() {
+		return noOfPages;
+	}
+
+	public void setNoOfPages(int noOfPages) {
+		this.noOfPages = noOfPages;
+	}
+
+	public int getEdition() {
+		return edition;
+	}
+
+	public void setEdition(int edition) {
+		this.edition = edition;
+	}
+
+	public int getChapters() {
+		return chapters;
+	}
+
+	public void setChapters(int chapters) {
+		
+		if(chapters > 0 && chapters < 10) {
+		this.chapters = chapters;
+		}
+		else {
+			System.out.println("Chapters not in range!!");
+		}
+	}
+
+	public char getStatus() {
+		return status;
+	}
+
+	public void setStatus(char status) {
+		this.status = status;
+	} 
+	
+	//Misc Methods
+	public void Borrow() {
+		setStatus('B');
+	}
+	
+	public void BookReturn() {
+		setStatus('A');
+	}
+
+	@Override
+	public String toString() {
+		return "Book [title=" + title + ", author=" + author + ", noOfPages=" + noOfPages + ", edition=" + edition
+				+ ", chapters=" + chapters + ", status=" + status + "]";
+	}
+	
+	
+
+}
